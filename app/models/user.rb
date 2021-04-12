7e1db8249e5acfe3590ee43eaefb675f86fdf35a
Addis Belete <addis.bel@gmail.com>
2021-04-12 16:37:48 +0300

    Migrate post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :name, presence:true, length: { minimum: 3, maximum: 20 }
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
 
-    has_many :posts
+  has_many :posts
 end
