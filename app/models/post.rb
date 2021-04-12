c051ae973841d8f0210f0aca1b9508f468f8a3c1
Addis Belete <addis.bel@gmail.com>
2021-04-12 20:05:00 +0300

    fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true, length: { minimum: 5, maximum: 100 }
